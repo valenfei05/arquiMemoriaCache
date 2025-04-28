97ba02d4cb78fb772c0f56a6b3a4de13606b5688
Daniel <danielp0sada@javerianacali.edu.co>
2025-04-28 11:38:36 -0500

    MOD: Eliminacion hits y misses Cache.

--- src/cache.h
+++ src/cache.h
@@ -6,15 +6,9 @@
 
 class Cache {
     private: 
-        int cantHits;
-        int cantMiss; 
         vector<Conjunto> conjuntos; 
     public:
         Cache(); 
-        void increaseHits(); 
-        void increaseMisses(); 
-        int getHits() const;
-        int getMisses() const;
 
         pair<bool, pair<int, int>> isHit(int tag); // Verifica si hay un hit en la cache, devuelve un par con el index y la vía
         unsigned char getValue(int index, int via, int offset); 
